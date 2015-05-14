d00d20a6719d463834f536e48e97fbe20c2eb160
Fedor Borshev <f@f213.in>
2015-05-14 22:20:06 +0300

    Multilang pluralisation

--- scripts/plural.js
+++ scripts/plural.js
@@ -1,5 +1,3 @@
 var Smart = require('smart-plurals');
 
-//var _plural = Smart.Plurals.getRule(hexo.config.lang);
-
-hexo.extend.helper.register('plural', Smart.Plurals.getRule('ru'));
+hexo.extend.helper.register('plural', Smart.Plurals.getRule(hexo.config.lang));
