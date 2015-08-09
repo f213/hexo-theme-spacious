271b160139380a5372daa6d8f2c6c75709b5c691
Fedor Borshev <f@f213.in>
2015-08-09 18:50:08 +0300

    HTML basic testing

--- test/test.js
+++ test/test.js
@@ -2,60 +2,94 @@ var chai            = require('chai'),
     should          = chai.should(),
     Hexo            = require('hexo'),
     del             = require('del'),
-    w3c             = require('w3cjs');
+    w3c             = require('w3cjs'),
+    fs              = require('fs'),
+    cheerio         = require('cheerio');
 
-var hexoInstallPath = process.cwd() + '/test/hexo/';
+var skipW3c         = false;
+var skipGenerate    = false;
+
+var w3cTimeout      = 10000; // ms
+
+var hexoInstallPath = process.cwd() + '/test/hexo/'; // slash required
+
+var path = function(name){
+    return hexoInstallPath + 'public/' + name;
+};
+var $load = function(file){
+    return cheerio.load(fs.readFileSync(path(file)), {xmlMode: true});
+};
+
+var $pages = {};
+
+var load$pages = function(){
+    $pages.home = $load('index.html');
+    $pages.post = $load('hello-world/index.html');
+}
 
 before(function(done){
-    // setup a hexo dir
-    this.timeout(15000);
 
-    del.sync(hexoInstallPath + '/public/*');
+    if(!skipGenerate){
 
-    var hexo = new Hexo(hexoInstallPath, {});
+        // setup a hexo dir may be sloow
+        this.timeout(15000);
 
-    hexo.init().then(function(){
-        return hexo.load();
-    })
-    .then(function(){
-        return hexo.call('generate', {});
-    })
-    .then(done);
-});
+        del.sync(hexoInstallPath + '/public/*');
 
-after(function(done){
-    del.sync(hexoInstallPath + '/public');
-    done();
+        var hexo = new Hexo(hexoInstallPath, {});
+
+        hexo.init().then(function(){
+            return hexo.load();
+        })
+        .then(function(){
+            return hexo.call('generate', {});
+        })
+        .then(function(){
+            load$pages();
+            return done();
+        });
+    }
+    else{
+        load$pages();
+        done();
+    }
 });
 
 describe('Home page', function(){
-    it('is w3c valid', function(done){
+    if(!skipW3c){
+        it('is w3c valid', function(done){
 
-        this.timeout(10000);
+            this.timeout(w3cTimeout);
 
-        w3c.validate({
-            file:       hexoInstallPath + '/public/index.html',
-            callback:   function (result){
-                console.log(result.messages);
-                result.messages.should.not.have.length.above(2); // By default, w3c gives two warning messages
-                done();
-            }
+            w3c.validate({
+                file:       path('index.html'),
+                callback:   function (result){
+                    console.log(result.messages);
+                    result.messages.should.not.have.length.above(2); // By default, w3c gives two warning messages
+                    done();
+                }
+            });
         });
+    }
+    it('has valid RSS link', function(){
+        $pages.home('link[rel="alternate"]').attr('href').should.be.eql('/rss/index.xml'); // тесты на HTML ебашить так.
     });
 });
 
 describe('Post page', function(){
-    it('is w3c valid', function(done){
+    if(!skipW3c){
+        it('is w3c valid', function(done){
 
-        this.timeout(10000);
+            this.timeout(w3cTimeout);
 
-        w3c.validate({
-            file:       hexoInstallPath + 'public/hello-world/index.html',
-            callback:   function(result){
-                console.log(result.messages);
-                result.messages.should.not.have.length.above(2); // By default, w3c gives two warning messages
-                done();
-            }
+            w3c.validate({
+                file:       path('hello-world/index.html'),
+                callback:   function(result){
+                    console.log(result.messages);
+                    result.messages.should.not.have.length.above(2); // By default, w3c gives two warning messages
+                    done();
+                }
+            });
         });
-    });
+    }
 });
