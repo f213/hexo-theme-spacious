87fa0cddfe431937d3b7dc9049b6d120f6c997d5
Fedor Borshev <f@f213.in>
2015-10-24 15:02:56 +0300

    Tooltips support, native on desktop, jquery.tooltipster on mobile.

--- source/js/mobile.js
+++ source/js/mobile.js
@@ -1,7 +1,13 @@
 (function(){
- if(window.matchMedia('(max-width: 640px)').matches){
-    $('body').addClass('mobile');
- }else{
-    $('body').addClass('no-mobile');
- }
+
+    if(window.matchMedia('(max-width: 640px)').matches){
+        $('body').addClass('mobile');
+    }else{
+        $('body').addClass('no-mobile');
+        return; // ↓↓↓ Mobile code follows ↓↓↓
+    }
+
+    $('span[title]').tooltipster({
+        trigger: 'click'
+    });
 })();
